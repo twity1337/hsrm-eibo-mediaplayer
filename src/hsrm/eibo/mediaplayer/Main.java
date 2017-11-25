624adf14df5413ed2b212ac784c9d8460c8f0748
Tim Wißmann <twity1337@gmail.com>
2017-11-26 00:57:35 +0100

    initial commit

---
+++ src/hsrm/eibo/mediaplayer/Main.java
@@ -0,0 +1,17 @@
+package hsrm.eibo.mediaplayer;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        System.out.println("Dies ist ein Test!");
+        System.exit(0);
+    }
+}
