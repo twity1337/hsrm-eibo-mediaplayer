b924a1920ef2bd0435597e09f2f2fa77674c0d5c
mapfl <marc.l.pflueger@student.hs-rm.de>
2017-11-30 23:58:20 +0100

    added MediaController class for handling of MediaPlayer logik added rudimentary scene graph for testing in main, player wont start yet, status unknown

--- src/hsrm/eibo/mediaplayer/Main.java
+++ src/hsrm/eibo/mediaplayer/Main.java
@@ -1,9 +1,12 @@
 package hsrm.eibo.mediaplayer;
 
-import hsrm.eibo.mediaplayer.Core.Model.Metadata;
 import javafx.application.Application;
 import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
 import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.Group;
 
 import java.net.URI;
 import java.util.ArrayList;
@@ -21,6 +24,19 @@ public class Main extends Application {
         Media mediaToTestMetaData = new Media(testFilePath);
         Track test = new Track(testFilePath);
         System.out.println(test.getMetadata().getAlbum());
-        System.exit(0);
+
+        primaryStage.setTitle("bla");
+        Group root = new Group();
+        Scene scene = new Scene(root);
+
+        MediaPlayer player = test.getTrackMediaPlayer();
+        MediaController mediaControl = MediaController.getInstance();
+        scene.setRoot(mediaControl);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        mediaControl.setTrack(player);
+        mediaControl.play();
     }
 }
