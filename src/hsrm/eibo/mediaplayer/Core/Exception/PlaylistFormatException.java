7a6cd070c592107c3b880c7ff32db16603d13fbc
mapfl <marc.l.pflueger@student.hs-rm.de>
2017-12-17 20:43:17 +0100

    commented PlaylistFormatException and PlaylistIOException

--- src/hsrm/eibo/mediaplayer/Core/Exception/PlaylistFormatException.java
+++ src/hsrm/eibo/mediaplayer/Core/Exception/PlaylistFormatException.java
@@ -1,4 +1,7 @@
 package hsrm.eibo.mediaplayer.Core.Exception;
 
+/**
+ * Exception thrown when playlist not loaded do to error in information contained in it
+ */
 public class PlaylistFormatException extends PlaylistException{
 }
