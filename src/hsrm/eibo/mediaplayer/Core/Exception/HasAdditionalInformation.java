dcfae9b46e677cb5b0b57a7adb39f02c449c0d8c
mapfl <marc.l.pflueger@student.hs-rm.de>
2017-12-17 20:30:36 +0100

    commented HasAdditionalInformation

--- src/hsrm/eibo/mediaplayer/Core/Exception/HasAdditionalInformation.java
+++ src/hsrm/eibo/mediaplayer/Core/Exception/HasAdditionalInformation.java
@@ -1,5 +1,8 @@
 package hsrm.eibo.mediaplayer.Core.Exception;
 
+/**
+ * This interface provides access to additional information in any Exception thrown by this application
+ */
 public interface HasAdditionalInformation {
 
     public String getAddionalInformationMessage();
