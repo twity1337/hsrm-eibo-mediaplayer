0936c346c4983d76f4363bcefef2cd475657d68d
mapfl <marc.l.pflueger@student.hs-rm.de>
2017-12-17 21:03:01 +0100

    wrote author note in main

--- src/hsrm/eibo/mediaplayer/Main.java
+++ src/hsrm/eibo/mediaplayer/Main.java
@@ -7,7 +7,16 @@ import javafx.stage.Stage;
 
 import java.util.Observable;
 
-
+/**
+ * @author Marc Lucas Pflueger
+ * @author Tim Wissmann
+ * This application is the work of both authors for the "Hochschule Rhein Main"
+ * course "Entwicklung interaktiver Benutzeroberflächen" WS 17/18.
+ * The task was to create an Mp3 Media player with javafx as the framework.
+ * The application should be designed with a design pattern taught in named course.
+ * We used the ModelViewController design to make the application and its components
+ * both extendable and interchangeable.
+ */
 public class Main extends Application {
 
     private static final boolean DEBUG_MODE_ENABLED = true;
