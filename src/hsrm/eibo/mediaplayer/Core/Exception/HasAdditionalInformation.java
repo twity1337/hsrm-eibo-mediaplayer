efb947f76ca724c8972d2d035d845043913fe9e8
mapfl <marc.l.pflueger@student.hs-rm.de>
2017-12-17 20:36:59 +0100

    fixed visibility of method in HasAdditionalInformation, commented MediaPlayerException

--- src/hsrm/eibo/mediaplayer/Core/Exception/HasAdditionalInformation.java
+++ src/hsrm/eibo/mediaplayer/Core/Exception/HasAdditionalInformation.java
@@ -1,9 +1,9 @@
 package hsrm.eibo.mediaplayer.Core.Exception;
 
 /**
- * This interface provides access to additional information in any Exception thrown by this application
+ * This interface provides access to additional information in Exception thrown by this application
  */
 public interface HasAdditionalInformation {
 
-    public String getAddionalInformationMessage();
+    String getAddionalInformationMessage();
 }
