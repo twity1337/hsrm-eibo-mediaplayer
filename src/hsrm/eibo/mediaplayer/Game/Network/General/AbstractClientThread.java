64a1dee58b50b35cc93f8488563f95859f6b04d9
Tim Wißmann <twity1337@gmail.com>
2018-01-17 19:26:07 +0100

    Added NetworkEventPacket model..

--- src/hsrm/eibo/mediaplayer/Game/Network/General/AbstractClientThread.java
+++ src/hsrm/eibo/mediaplayer/Game/Network/General/AbstractClientThread.java
@@ -1,7 +1,5 @@
 package hsrm.eibo.mediaplayer.Game.Network.General;
 
-import org.apache.commons.lang.ArrayUtils;
-
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
@@ -31,7 +29,8 @@ public abstract class AbstractClientThread extends Thread {
     public synchronized void pushToProcessingQueue(byte[] data) {
 
         // add timecode prefix to data before checking packet length..
-        byte[] processedData = ArrayUtils.addAll(ArrayUtils.addAll(this.generateTimeCode(), new byte[]{0,0}), data);
+        byte[] processedData = data;
+//        byte[] processedData = ArrayUtils.addAll(ArrayUtils.addAll(this.generateTimeCode(), new byte[]{0,0}), data);
 
         if(processedData.length >= AbstractSocketManager.PACKET_LENGTH)
         {
@@ -60,16 +59,5 @@ public abstract class AbstractClientThread extends Thread {
     protected boolean hasDataToProcess()
     {
         return dataProcessingQueue.isEmpty();
-    }
-
-    private byte[] generateTimeCode()
-    {
-        long currentTime = System.currentTimeMillis();
-        return new byte[] {
-            (byte) (currentTime >> 24),
-            (byte) (currentTime >> 16),
-            (byte) (currentTime >> 8),
-            (byte) currentTime
-        };
     }
 }
--- src/hsrm/eibo/mediaplayer/Game/Network/General/Event/NetworkPacketEventDecoder.java
+++ src/hsrm/eibo/mediaplayer/Game/Network/General/Event/NetworkPacketEventDecoder.java
@@ -1,8 +1,9 @@
 package hsrm.eibo.mediaplayer.Game.Network.General.Event;
 
 /**
- * Receives a network packet as byte array, extracts timestamp, command and its args of the packet and calls the
+ * Receives network packets as byte array, extracts timestamp, command and its args of the packet and calls the
  * appropriate event handler.
  */
 public class NetworkPacketEventDecoder {
+
 }
